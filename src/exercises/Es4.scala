dcca01e34e70c3e08eb4dbcbfe0905f95a37207f
Michele Bachetti <michele.bachetti@studio.unibo.it>
2022-03-09 18:58:15 +0100

    Completed exercise 4

---
+++ src/exercises/Es4.scala
@@ -0,0 +1,12 @@
+package exercises
+
+class Es4 {
+
+  val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y <= z
+
+  val p2: (Int, Int, Int) => Boolean = (x,y,z) => x <= y && y <= z
+
+  def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y <= z
+
+  def p4(x: Int, y: Int, z: Int): Boolean = x <= y && y <= z
+}
